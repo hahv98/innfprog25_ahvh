abdced128041a2818b3ad43171d5f02c883930ee
hahv98 <henden.ahv@gmail.com>
2025-12-03 16:34:56 +0100

    utfylling av oppg 1 og 3

--- eksamen_innfprog2025/oppgave1/actions.js
+++ eksamen_innfprog2025/oppgave1/actions.js
@@ -1,3 +1,46 @@
-function registerInfo() {
-    return
+import {staff, admin, teach, allKurs} from "./register.js";
+
+document.getElementById("kursliste").innerHTML = allKurs.map(kurs => `<li>${kurs}</li>`).join("");
+
+const container = document.getElementById("output")
+function visAnsatte(list) {
+    container.innerHTML = "";
+
+    list.forEach(a => {
+        const kort = document.createElement("div");
+        kort.className = "kort";
+        
+        kort.innerHTML = `
+            <h3>${a.fornavn} ${a.etternavn}</h3>
+            <p><b>Stilling:</b> ${a.stilling}</p>
+            <p><b>Kontor:</b> ${a.kontor}</p>
+            <p><b>E-post:</b> <a href="mailto:${a.epost}">${a.epost}</a></p>
+            ${a.avdeling ? `<p><b>Kursansvar:</b> ${a.avdeling}</p>` : "<em>Ingen kursansvar</em>"}
+        `;
+
+        container.appendChild(kort);
+    });
 }
+
+function filter(type) {
+    if (type === "alle") {
+        visAnsatte(staff);
+        return;
+    }
+    else if (type === "administrasjon") {
+        const resultat = staff.filter(a => admin.includes(a.stilling));
+        visAnsatte(resultat);
+        return;
+    }
+    else if (type === "undervisere") {
+        const resultat = staff.filter(a => teach.includes(a.stilling));
+        visAnsatte(resultat);
+        return;
+    }
+}
+document.querySelectorAll(".filter-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        filter(btn.dataset.filter);
+    });
+});
+visAnsatte(staff);
--- eksamen_innfprog2025/oppgave1/register.js
+++ eksamen_innfprog2025/oppgave1/register.js
@@ -1,169 +1,152 @@
-const staff = {
-    rektor: [
+export const allKurs = [
+    "Programmering 1", "Webutvikling", "Algoritmer og datastrukturer",
+    "Kunstig intelligens", "Maskinlæring", "Databaser",
+    "Operativsystemer", "Webdesign", "Responsiv utvikling",
+    "Grunnleggende JavaScript", "UX-design", "Frontend-utvikling",
+    "CSS og design", "Python for web", "Systemutvikling",
+    "Objektorientert programmering", "Datakommunikasjon",
+    "Datasikkerhet", "Prosjektledelse"
+]
+// Programmering 1 = allKurs[0]                   | Webutvikling = allKurs[1]       | Algoritmer og datastrukturer = allKurs[2]
+// Kunstig intelligens = allKurs[3]               | Maskinlæring = allKurs[4]       | Databaser = allKurs[5]
+// Operativsystemer = allKurs[6]                  | Webdesign = allKurs[7]          | Responsiv utvikling = allKurs[8]
+// Grunnleggende JavaScript = allKurs[9]          | UX-design = allKurs[10]         | Frontend-utvikling = allKurs[11]
+// CSS og design = allKurs[12]                    | Python for web = allKurs[13]    | Systemutvikling = allKurs[14]
+// Objektorientert programmering = allKurs[15]    | Datakommunikasjon = allKurs[16]
+// Datasikkerhet = allKurs[17]                    | Prosjektledelse = allKurs[18]
+
+export const staff = [
         {
+            stilling: "Rektor", kontor: "R-001", avdeling: "", 
             etternavn: "Solheim", 
-            fornavn: "Ingrid", 
-            kontor: "R-001",  
+            fornavn: "Ingrid",   
             epost: "ingrid.solheim@prog.no", 
-            avdeling: "" 
-        }
-    ],
-    dekan: [
+        },
         { 
+            stilling: "Dekan", kontor: "D-101", avdeling: "",
             etternavn: "Aas", 
             fornavn: "Morten", 
-            kontor: "D-101",
             epost: "morten.aas@prog.no", 
-            avdeling: "" 
         },
         { 
+            stilling: "Dekan", kontor: "D-102", avdeling: "",
             etternavn: "Nergård", 
             fornavn: "Selma", 
-            kontor: "D-102",
-            epost: "selma.nergard@prog.no", 
-            avdeling: "" 
-        }
-    ],
-    vaktmester: [
+            epost: "selma.nergard@prog.no",  
+        },
         { 
+            stilling: "Vaktmester", kontor: "V-201", avdeling: "",
             etternavn: "Eriksen", 
             fornavn: "Tom", 
-            kontor: "V-201", 
             epost: "tom.eriksen@prog.no", 
-            avdeling: "" 
         },
         { 
+            stilling: "Vaktmester", kontor: "V-202", avdeling: "",
             etternavn: "Kahn",
             fornavn: "Nabila", 
-            kontor: "V-202", 
             epost: "nabila.kahn@prog.no",
-            avdeling: "" 
         },
         {
-            kontor: "V-203", 
-            fornavn: "Jonas", 
+            stilling: "Vaktmester", kontor: "V-203", avdeling: "",
             etternavn: "Røed", 
+            fornavn: "Jonas", 
             epost: "jonas.roed@prog.no",
-            avdeling: ""
-        } 
-    ],
-    professor: [
+        },
         { 
+            stilling: "Professor", kontor: "P-301", avdeling: `${allKurs[1]}, ${allKurs[7]}`,
             etternavn: "Wendelstad", 
             fornavn: "Eirik", 
-            kontor: "P-301", 
             epost: "eirik.wendelstad@prog.no", 
-            avdeling: ["Informatikk", "programmering"]
         },
         { 
+            
+            stilling: "Professor", kontor: "P-302", avdeling: `${allKurs[3]}`,
             etternavn: "Fosse", 
-            fornavn: "Maria", 
-            kontor: "P-302", 
+            fornavn: "Maria",
             epost: "maria.fosse@prog.no",
-            avdeling: "Pedagogikk", 
         },
         { 
-            etternavn: "Tran", 
-            fornavn: "Linh", 
-            kontor: "P-303", 
+            stilling: "Professor", kontor: "P-303", avdeling: `${allKurs[4]}, ${allKurs[17]}`,
+            etternavn: "Tran",
+            fornavn: "Linh",
             epost: "linh.tran@prog.no",
-            avdeling: "Fysikk", 
         },
         { 
-            etternavn: "Kleppe", 
+            stilling: "Professor", kontor: "P-304", avdeling: `${allKurs[5]}`,
+            etternavn: "Kleppe",
             fornavn: "Gunnar", 
-            kontor: "P-304", 
-            epost: "gunnar.kleppe@prog.no",
-            avdeling: "Historie", 
+            epost: "gunnar.kleppe@prog.no" 
         }, 
         { 
+            stilling: "Professor", kontor: "P-305", avdeling: `${allKurs[6]}`,
             etternavn: "Bergmann", 
-            fornavn: "Sofia", 
-            kontor: "P-305", 
-            epost: "sofia.bergmann@prog.no",
-            avdeling: "Biologi", 
-        }
-    ],
-    lektor: [
+            fornavn: "Sofia",
+            epost: "sofia.bergmann@prog.no"
+        },
         {
+            stilling: "Lektor", kontor: "L-401", kontor: "L-401", avdeling: `${allKurs[7]}, ${allKurs[12]}`,
             etternavn: "Nymo", 
-            fornavn: "Astrid", 
-            kontor: "L-401", 
-            epost: "astrid.nymo@prog.no",
-            avdeling: "Engelsk", 
+            fornavn: "Astrid",
+            epost: "astrid.nymo@prog.no"
         },
         { 
+            stilling: "Lektor", kontor: "L-402", avdeling: `${allKurs[8]}`,
             etternavn: "Liland", 
-            fornavn: "Henrik", 
-            kontor: "L-402", 
-            epost: "henrik.liland@prog.no",
-            avdeling: ["Matematikk", "Kjemi"] 
+            fornavn: "Henrik",
+            epost: "henrik.liland@prog.no"
         },
         { 
+            stilling: "Lektor", kontor: "L-403", avdeling: `${allKurs[9]}, ${allKurs[13]}`,
             etternavn: "Strand",
-            fornavn: "Julie", 
-            kontor: "L-403", 
-            epost: "julie.strand@prog.no",
-            avdeling: ["Samfunnsfag", "Geografi"] 
+            fornavn: "Julie",
+            epost: "julie.strand@prog.no" 
         },
         { 
+            stilling: "Lektor", kontor: "L-404", avdeling: `${allKurs[10]}`,
             etternavn: "Borge",
-            fornavn: "Samuel", 
-            kontor: "L-404", 
-            epost: "samuel.borge@prog.no",
-            avdeling: "Programmering", 
+            fornavn: "Samuel",
+            epost: "samuel.borge@prog.no" 
         },
         { 
+            stilling: "Lektor", kontor: "L-405", avdeling: `${allKurs[11]}`,
             etternavn: "Heggernes",
-            fornavn: "Kari", 
-            kontor: "L-405", 
-            epost: "kari.heggernes@prog.no",
-            avdeling: "Kunst og håndverk", 
+            fornavn: "Kari",
+            epost: "kari.heggernes@prog.no"
         },
         { 
+            stilling: "Lektor", kontor: "L-406", avdeling: `${allKurs[12]}`,
             etternavn: "Røine",
-            fornavn: "Daniel", 
-            kontor: "L-406", 
-            epost: "daniel.roine@prog.no",
-            avdeling: "Norsk", 
+            fornavn: "Daniel",
+            epost: "daniel.roine@prog.no" 
         },  
         { 
+            stilling: "Lektor", kontor: "L-407", avdeling: `${allKurs[14]}, ${allKurs[18]}`,
             etternavn: "Arctander",
-            fornavn: "Nina", 
-            kontor: "L-407", 
-            epost: "nina.arctander@prog.no",
-            avdeling: "Spansk", 
+            fornavn: "Nina",
+            epost: "nina.arctander@prog.no"
         },
         { 
+            stilling: "Lektor", kontor: "L-408", avdeling: `${allKurs[13]}`,
             etternavn: "Jebe",
-            fornavn: "Oskar", 
-            kontor: "L-408", 
-            epost: "oskar.jebe@prog.no",
-            avdeling: "Fysikk", 
+            fornavn: "Oskar",
+            epost: "oskar.jebe@prog.no" 
         },
         { 
+            stilling: "Lektor", kontor: "L-409", avdeling: `${allKurs[15]}, ${allKurs[1]}`,
             etternavn: "Vik", 
-            fornavn: "Thea", 
-            kontor: "L-409", 
-            epost: "thea.vik@prog.no",
-            avdeling: "Geografi", 
+            fornavn: "Thea",
+            epost: "thea.vik@prog.no"
         },
         { 
+            stilling: "Lektor", kontor: "L-410", avdeling: `${allKurs[16]}`,
             etternavn: "Halvorsen", 
-            fornavn: "Emil", 
-            kontor: "L-410", 
-            epost: "emil.halvorsen@prog.no",
-            avdeling: "Kjemi" 
+            fornavn: "Emil",
+            epost: "emil.halvorsen@prog.no"
         }
     ]
-}
 
 
+export const admin = ["Rektor", "Dekan", "Vaktmester"];
+export const teach = ["Professor", "Lektor"];
 
-const allStaff = [
-    ...staff.rektor,
-    ...staff.dekan,
-    ...staff.vaktmester,
-    ...staff.professor,
-    ...staff.lektor
-]
+
